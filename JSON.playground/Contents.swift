84f58743fffada3ca1121d21c5ac6e418c650494
Morgan Hondros <morganhondros@gmail.com>
2019-06-22 19:41:45 -0400

    begining of lecture 14. nested dictionary and such.

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -1,53 +1,41 @@
 import UIKit
 
-struct PlacesResponse : Decodable {
-    var places : [Place]
-}
 
-struct Place : Decodable {
-    var name : String
-    var latitude : Double
-    var longitude : Double
-
-}
 
 let json = """
-{
-    "places": [
-    {
-        "name" : "Costa Rica",
-        "latitude" : 34.56,
-        "longitude" : 65.67
-    },
-    {
-        "name" : "Boston",
-        "latitude" : 134.56,
-        "longitude" : 265.67
-    }
-    
-    ]
-}
 
+    {
+        "customers": [
+        {
+            "firstName" : "John",
+            "lastName" : "Doe",
+            "address" : {
+                "street" : "1200 Richmond Ave",
+                "city" : "Houston",
+                "state" : "TX",
+            "geo" : {
+                "latitude" : 34.56,
+                "longitude" : 35.65
+            }
+        }
+    }
+]
+}
 
 """.data(using: .utf8)!
 
-let placesResponse = try! JSONDecoder().decode(PlacesResponse.self, from: json)
-print(placesResponse.places)
 
 
-//let placesDictionary = try! JSONDecoder().decode([String:[Place]].self, from: json)
-//let places = placesDictionary["places"]
+
+
+//import UIKit
 //
-//print(places)
-
-
-    
-
-
-
-
-
-
-
+//
+//
+//let json = """
+//
+//
+//
+//""".data(using: .utf8)!
 
 

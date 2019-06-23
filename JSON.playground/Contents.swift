800478bc3e710f6b6bbdf14ad78fd388a8db9c4d
Morgan Hondros <morganhondros@gmail.com>
2019-06-23 14:24:49 -0400

    End of lecture 23. JSON and struct in place.

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -10,18 +10,26 @@ let jsonData : Data = """
 "username" : "Bret"
 "email" : "Sincere@april.biz"
 "address" : {
-"street" : "Kulas Light"
-"suite": "Apt. 556",
-"city": "Gwenborough",
-"zipcode" : "92998-3874",
-
+    "street" : "Kulas Light"
+    "suite": "Apt. 556",
+    "city": "Gwenborough",
+    "zipcode" : "92998-3874",
 }
 }
 
 
 """.data(using: .utf8)!
 
-
+struct User : Decodable {
+    let id : Int
+    let name : String
+    let userName : String
+    let email : String
+    let street : String
+    let suite : String
+    let city : String
+    let zipCode : String
+}
 
 
 

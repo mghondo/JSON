381ae8dc00524647aa0b06d32700f219b33bea06
Morgan Hondros <morganhondros@gmail.com>
2019-06-23 14:21:01 -0400

    Begining of Lecture 23.

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -2,6 +2,25 @@ import UIKit
 
 
 
+let jsonData : Data = """
+
+{
+"id" : 1,
+"name": "Leanne Graham"
+"username" : "Bret"
+"email" : "Sincere@april.biz"
+"address" : {
+"street" : "Kulas Light"
+"suite": "Apt. 556",
+"city": "Gwenborough",
+"zipcode" : "92998-3874",
+
+}
+}
+
+
+""".data(using: .utf8)!
+
 
 
 

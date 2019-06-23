a1f44790d9f1f3ebc6e0720bc1d2ab7bc723a8cc
Morgan Hondros <morganhondros@gmail.com>
2019-06-23 19:43:21 -0400

    End of lecture 26. URL loaded and JSON info running through.

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -1,4 +1,22 @@
 import UIKit
+import PlaygroundSupport
+
+let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+
+URLSession.shared.dataTask(with: url) { data, response, error in
+    guard error == nil,
+        let data = data else {
+            print(error)
+            return
+    }
+    
+    print(data)
+    
+}.resume()
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+
 
 
 

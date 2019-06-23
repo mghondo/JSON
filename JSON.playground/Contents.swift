a54a255d64b9f01300eacbc22130c2d1e929766a
Morgan Hondros <morganhondros@gmail.com>
2019-06-22 22:48:16 -0400

    End of lecture 18. Adding address sub level functionality.

--- JSON.playground/Contents.swift
+++ JSON.playground/Contents.swift
@@ -1,17 +1,43 @@
 import UIKit
 
+struct Address : Decodable {
+    var street : String
+    var state : String
+    var zipCode : String
+    
+    private enum CodingKeys : String, CodingKey {
+        case street = "STREET"
+        case state = "STATE"
+        case zipCode = "ZIPCODE"
+    }
+    
+}
+
 
 struct Customer : Decodable {
     var firstName : String
     var lastName : String
     var age : Int
+    var address : Address?
+    
+    private enum CodingKeys : String, CodingKey {
+        case firstName = "FIRSTNAME"
+        case lastName = "LASTNAME"
+        case age = "AGE"
+        case address = "ADDRESS"
+    }
 }
 
 let json = """
 {
-    "first_name" : "John",
-    "last_name" : "Doe",
-    "age" : 34
+    "FIRSTNAME" : "John",
+    "LASTNAME" : "Doe",
+    "AGE" : 34,
+    "ADDRESS" : {
+        "STREET" : "1200 Richmond Ave",
+        "STATE" : "TX",
+        "ZIPCODE" : "77042"
+    }
 }
 
 """.data(using: .utf8)!
@@ -21,7 +47,7 @@ decoder.keyDecodingStrategy = .convertFromSnakeCase
 
 let customer = try! decoder.decode(Customer.self, from: json)
 
-print(customer)
+print(customer.address?.state)
 
 
 
